dd4e4fe452283ac6805845757e17d64ce3a69fcb
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2019-03-25 15:37:15 -0400

    add lander CTAs

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,6 @@
 import { API } from "aws-amplify";
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import "./Home.css";
@@ -63,6 +64,14 @@ export default class Home extends Component {
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
