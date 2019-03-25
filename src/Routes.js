2d98988cb0fab8fd21140bf9031f424f84ce5ca6
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2019-03-25 15:00:49 -0400

    add ability to display and update a note

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes"
 import NotFound from "./containers/NotFound";
 import React from "react";
 import { Route, Switch } from "react-router-dom";
@@ -12,5 +13,6 @@ export default ({ childProps }) =>
   <Route path="/login" exact render={props => <Login {...props} {...childProps} />} />
   <Route path="/signup" exact render={props=> <Signup {...props} {...childProps} />} />
   <Route path="/notes/new" exact render={props=> <NewNote {...props} {...childProps} />} />
+  <Route path="/notes/:id" exact render={props=> <Notes {...props} {...childProps} />} />
   <Route component={NotFound} />
 </Switch>;
